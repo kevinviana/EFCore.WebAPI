36d7194de22c0fc242eca8ce1fd1918facb477ad
Kevin Viana Pereira <vianakevinc2@hotmail.com>
2021-01-16 16:43:44 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore.WebAPI/Data/HeroContext.cs
@@ -0,0 +1,31 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroContext : DbContext
+    {
+        public DbSet<Hero> Heros { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<HeroBattle> HeroBattles { get; set; }
+        public DbSet<SecretIdentity> SecretIdentities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Password=@senhafoda2021;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-VPO5MAN\SQLEXPRESS");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroBattle>(entity =>
+            {
+                entity.HasKey(e => new { e.BattleId, e.HeroId });
+            });
+        }
+    }
+}
---
+++ EFCore.WebAPI/Models/Battle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descritption { get; set; }
+        public DateTime DtStart { get; set; }
+        public DateTime DtEnd { get; set; }
+        public List<HeroBattle> HeroBattles { get; set; }
+    }
+}
